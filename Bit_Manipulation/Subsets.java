2d89743f32647e27583ba6a95480f986fe449471
Ranjeet <ranjeet127ad@gmail.com>
2025-11-14 23:12:30 +0530

    Solved Subsets

---
+++ Bit_Manipulation/Subsets.java
@@ -0,0 +1,32 @@
+package Bit_Manipulation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+     public static List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        int total = 1 << n;   // 2^n
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int mask = 0; mask < total; mask++) { 
+            List<Integer> subset = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    subset.add(nums[i]);
+                }
+            }
+
+            result.add(subset);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        List<List<Integer>> ans = subsets(nums);
+        System.out.println(ans);
+    }
+}
