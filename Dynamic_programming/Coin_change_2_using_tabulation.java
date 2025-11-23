6a78c8c09179045a437660228ce4f2353858582f
Ranjeet <ranjeet127ad@gmail.com>
2025-11-23 23:45:23 +0530

    Solved Coin change 2 using tabulation

--- Dynamic_programming/Coin_change_2_using_tabulation.java
+++ Dynamic_programming/Coin_change_2_using_tabulation.java
@@ -1,14 +1,29 @@
 package Dynamic_programming;
 
-import java.util.Arrays;
-
 public class Coin_change_2_using_tabulation {
 
     public static int change(int amount, int[] coins) {
-        int[] dp = new int[coins.length];
-        Arrays.fill(dp,-1);
+        int n = coins.length;
+        int[][] dp = new int[n+1][amount+1];
 
-        return dp[amount];
+        for(int i=0; i<n; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=1; j<=amount; j++){
+
+                int notPick = dp[i+1][j];
+                int pick = 0;
+                
+                if(coins[i] <= j){
+                    pick = dp[i][j-coins[i]];
+                }
+                dp[i][j] = pick + notPick;
+            }
+        }
+
+        return dp[0][amount];
     }
     public static void main(String[] args) {
         int amount = 5;
